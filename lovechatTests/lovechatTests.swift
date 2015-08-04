dd99a0a29775b44c7833d08f7c1c03ec55e84a29
王聪 <2441413514@qq.com>
2015-08-04 15:47:00 +0800

    Initial Commit

---
+++ lovechatTests/lovechatTests.swift
@@ -0,0 +1,36 @@
+//
+//  lovechatTests.swift
+//  lovechatTests
+//
+//  Created by 王聪 on 8/4/15.
+//  Copyright (c) 2015 旅行派. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class lovechatTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
