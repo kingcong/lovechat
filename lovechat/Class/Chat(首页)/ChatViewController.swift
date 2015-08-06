982856dde8963f48a2c78d41c3b4690d730565ac
王聪 <2441413514@qq.com>
2015-08-06 15:02:28 +0800

    获取好友列表

---
+++ lovechat/Class/Chat(首页)/ChatViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChatViewController.swift
+//  lovechat
+//
+//  Created by 王聪 on 15/8/6.
+//  Copyright (c) 2015年 旅行派. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- lovechat/Class/Contact(联系人)/ContactListViewController.swift
+++ lovechat/Class/Contact(联系人)/ContactListViewController.swift
@@ -8,26 +8,66 @@
 
 import UIKit
 
-class ContactListViewController: UITableViewController {
+class ContactListViewController: UIViewController, UITableViewDataSource,UITableViewDelegate{
 
-    lazy var dataSource = [EMBuddy]()
+    var dataSource = [EMBuddy]()
+    
+    var tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.reloadDataSource()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.frame = self.view.bounds
+        self.view.addSubview(self.tableView)
+        
     }
     
     func reloadDataSource(){
         
+        // 移除数据
         self.dataSource.removeAll(keepCapacity: true)
         
-        var buddyList: Array = ease.chatManager.buddyList
-        var blockList: Array = ease.chatManager.blockedList
+        // 异步获取好友列表
+        ease.chatManager.asyncFetchBuddyListWithCompletion({ (buddyList, error: EMError!) -> Void in
+            if !(error != nil) {
+                println("获取成功\(buddyList.first)")
+                
+                // var buddy = buddyList[0] as! EMBuddy
+                for buddy: EMBuddy in buddyList as! Array{
+                    println("\(buddy.username)")
+                    self.dataSource.append(buddy)
+                }
+                
+                self.tableView.reloadData()
+            }
+            }, onQueue: nil)
         
-        for buddy: EMBuddy in buddyList {
-//            if blockList
-        }
+        println("\(self.dataSource)")
+        
+
+    }
+    
+    // MARK: 实现数据源方法
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dataSource.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+        let buddy = self.dataSource[indexPath.row]
+        cell.textLabel?.text = buddy.username
+        
+        return cell
+    }
+    
+    // MARK: 实现tableView的代理方法
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
     }
 
 }
